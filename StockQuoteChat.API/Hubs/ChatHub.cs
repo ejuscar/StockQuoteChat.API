8ccff444635163604b9bb828e8eb71f6ab0619bd
Eliézer Carmo <eliezerjuscar@gmail.com>
2023-01-10 01:31:23 -0300

    ChatHub updates to allow saving messages and get usernames by logged user

--- StockQuoteChat.API/Hubs/ChatHub.cs
+++ StockQuoteChat.API/Hubs/ChatHub.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using StockQuoteChat.API.Models;
-using System;
-using System.Net.Http;
+using StockQuoteChat.Application.Entities;
+using StockQuoteChat.Application.Models;
+using StockQuoteChat.Application.Models.Responses;
+using StockQuoteChat.Infrastructure.Repositories.Interfaces;
 using System.Text;
 using System.Text.Json;
 
@@ -14,35 +17,44 @@ namespace StockQuoteChat.API.Hubs
         private readonly string _command;
         private readonly IDictionary<string, UserConnection> _connections;
         private readonly HttpClient _httpClient;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IUserRoomRepository _userRoomRepository;
 
-        public ChatHub(IDictionary<string, UserConnection> connections)
+        public ChatHub(IDictionary<string, UserConnection> connections, IMessageRepository messageRepository, IUserRoomRepository userRoomRepository)
         {
             _botUser = "MyChat Bot";
             _connections = connections;
             _command = "/stock=";
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri("https://localhost:7047");
+            _messageRepository = messageRepository;
+            _userRoomRepository = userRoomRepository;
         }
 
+        [Authorize]
         public override Task OnDisconnectedAsync(Exception? exception)
         {
-            if(_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
             {
                 _connections.Remove(Context.ConnectionId);
-                Clients.Group(userConnection.Room)
-                    .SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has left");
+                Clients.Group(userConnection.Room.Id.ToString())
+                    .SendAsync("ReceiveMessage", new ChatMessageResponseDto($"{userConnection.User.FirstName} {userConnection.User.LastName} has left", _botUser));
 
-                Groups.RemoveFromGroupAsync(Context.ConnectionId, userConnection.Room);
+                Groups.RemoveFromGroupAsync(Context.ConnectionId, userConnection.Room.Id.ToString());
             }
             return base.OnDisconnectedAsync(exception);
         }
 
+        [Authorize]
         public async Task SendMessage(string message)
         {
             if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
             {
-                await Clients.Group(userConnection.Room)
-                    .SendAsync("ReceiveMessage", userConnection.User, message);
+                Message messageEntity = new Message(message, userConnection);
+                await _messageRepository.Insert(messageEntity);
+
+                await Clients.Group(userConnection.Room.Id.ToString())
+                    .SendAsync("ReceiveMessage", new ChatMessageResponseDto(messageEntity, userConnection.User.GetUserName()));
 
                 SendCommand(message, userConnection);
             }
@@ -51,15 +63,35 @@ namespace StockQuoteChat.API.Hubs
         [Authorize]
         public async Task JoinRoom(UserConnection userConnection)
         {
-            await AddUserToRoom(userConnection);
-            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+            if (_userRoomRepository.Insert(userConnection.User, userConnection.Room))
+            {
+                await AddUserToRoom(userConnection);
+
+                var messagesLoaded = _messageRepository.GetByRoom(userConnection.Room.Id)
+                    .Include(m => m.UserRoom.User)
+                    .Select(m => new ChatMessageResponseDto(m, m.UserRoom.User.GetUserName()))
+                    .ToList();
+
+                await Clients
+                    .Clients(Context.ConnectionId)
+                    .SendAsync("LoadMessages", messagesLoaded);
+
+                await Clients
+                    .Group(userConnection.Room.Id.ToString())
+                    .SendAsync("ReceiveMessage", new ChatMessageResponseDto($"{userConnection.User.GetUserName()} has joined {userConnection.Room.Name}", _botUser));
+            }
+
+            else
+            {
+                throw new Exception("Error while trying to join in room");
+            }
         }
 
+        [Authorize]
         public async Task AddUserToRoom(UserConnection userConnection)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room.Id.ToString());
             _connections[Context.ConnectionId] = userConnection;
-            
         }
 
         private async Task SendCommand(string message, UserConnection userConnection)
@@ -72,16 +104,16 @@ namespace StockQuoteChat.API.Hubs
                     ?.Replace(_command, "")!;
 
                 if (string.IsNullOrEmpty(stockCode))
-                    await Clients.Group(userConnection.Room)
+                    await Clients.Group(userConnection.Room.Id.ToString())
                         .SendAsync("ReceiveMessage", _botUser, "Stock Code cannot be empty.");
 
-                ChatBotRequestDto chatBotRequest = new ChatBotRequestDto(userConnection.Room, stockCode);
+                ChatBotRequestDto chatBotRequest = new ChatBotRequestDto(userConnection.Room.Id.ToString(), stockCode);
 
                 var requestContent = new StringContent(JsonSerializer.Serialize(chatBotRequest), Encoding.UTF8, "application/json");
                 var response = await _httpClient.PostAsync("chatbot", requestContent);
 
                 if (!response.IsSuccessStatusCode)
-                    await Clients.Group(userConnection.Room)
+                    await Clients.Group(userConnection.Room.Id.ToString())
                         .SendAsync("ReceiveMessage", _botUser, "An error occurred while trying to get the stock quote. Try again later.");
             }
         }
